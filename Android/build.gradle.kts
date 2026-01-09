5301cb4cbf1425b3c48a8a1dc810cb6b01812fab
Pulkit Midha <pulkit.midha@couchbase.com>
2026-01-09 18:52:29 +0530

    fix Gradle plugin resolution and SSL certificate validation

--- Android/build.gradle.kts
+++ Android/build.gradle.kts
@@ -1,5 +1,18 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://mobile.maven.couchbase.com/maven2/dev/")
+            isAllowInsecureProtocol = false
+        }
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.5.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
+        classpath("org.jetbrains.kotlin:compose-compiler-gradle-plugin:2.0.21")
+    }
 }
+
+// Plugins are applied in the app module
